9f05a3615ba988c0f5141d8e57e79f209acb2a05
Adham Walaa Elewa <adham.elewa@student.giu-uni.de>
2025-11-11 09:48:26 +0200

    Add Course class with student management methods

--- Course.h
+++ Course.h
@@ -4,6 +4,23 @@
 #include <string>
 #include "Student.h"
 using namespace std;
+class Course:: Person {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    addStudent(const Student& s) {
+
+    }
+    displayCourseInfo () {
+        cout<< "Course: " << courseCode << " - " << courseName<< endl << " Max Students: " << maxStudents << endl <<
+            "Currently Enrolled: " << Student->display() << endl << "Instructor Info:" << endl << Instructor->display()
+        << endl << "Student Info:" << endl << Student->display();
+    }
+};
 //#write your code here
 
 
