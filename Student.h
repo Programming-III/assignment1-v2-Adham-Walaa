9799c7a7bca7433f410117fc27b72bbfaba28f69
Adham Walaa Elewa <adham.elewa@student.giu-uni.de>
2025-11-11 09:46:18 +0200

    Implement Student class with attributes and methods
    Added Student class with year level and major attributes, along with methods to retrieve these values and display student information.

--- Student.h
+++ Student.h
@@ -4,6 +4,25 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
+class Student:: Person {
+        private:
+        int yearLevel;
+        string major;
+        public:
+        Student(int y, string m) {
+            yearLevel=y;
+            major=m;
+        }
+        int getYearLevel () {
+            return yearLevel;
+        }
+        string getMajor () {
+            return major;
+        }
+        display() {
+            cout<<"Name: " << Person->getName() << endl << "Year Level: " << yearLevel << endl << "Major:" << major;
+        }
+    };
 //#write student class here
 
 
