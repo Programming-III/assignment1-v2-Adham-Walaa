3d7aea03083fb3798f486b28e7722a71835d9f0b
Adham Walaa Elewa <adham.elewa@student.giu-uni.de>
2025-11-11 09:47:20 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,6 +3,27 @@
 
 #include "Person.h"
 using namespace std;
+class Instructor:: Person {
+    private:
+        string department;
+        int experienceYears;
+    public:
+        Instructor (string p, int e) {
+            department=p;
+            experienceYears=e;
+        }
+        string getDepartment () {
+            return department;
+        }
+        int getExperienceYears () {
+            return experienceYears;
+        }
+        display() {
+            cout<<"Name: " << Person->getName() << endl << "Department: " << department << endl <<
+                "Experience Years: " << experienceYears;
+        }
+
+    };
 //#write Instructor class here
 
 
